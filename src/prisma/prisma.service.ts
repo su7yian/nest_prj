1c7ba2b11b99fff8f5f44e2fee5a6b4cb96642d8
Sufyian <su7yian@gmail.com>
2026-06-23 10:09:31 +0500

    before jwt

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '../generated/prisma/client.js';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL as string,
+      connectionString: config.get<string>('DATABASE_URL'),
     });
     super({ adapter });
   }
